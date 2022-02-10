c56246db642a31dacc9265b6802d763c1abecae8
Albert Sun <albertsun0@gmail.com>
2022-02-09 18:57:20 -0600

    a

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import Location from '../assets/location.png'
 function AboutSection() {
   return (
     <div className = 'flex flex-col'>
+      <div className = "text-center p-8"><strong>The Concert Hall: </strong> The main concert hall features a “vineyard” seating pattern with the performers in the center and the audience in the same area. It does not have booths or elevated seating.</div>
       <div className='p-10 flex flex-row w-full'>
       <div className='flex flex-col p-20 w-1/2'>
         <div className='text-4xl mb-14' id='about'>
@@ -45,16 +46,27 @@ function AboutSection() {
         </div>
       </div>
     </div>
-    <div className = 'px-20 py-10 text-2xl'>Architectural Drawings</div>
+    <div className = 'px-20 py-10 text-4xl'>Architectural Drawings</div>
     <div className = 'flex flex-row px-20'>
       <img className = 'w-1/2 p-10' src = 'http://wdch10.laphil.com/image/356/564/375'></img>
       <img className = 'w-1/2 p-10' src = 'http://wdch10.laphil.com/image/357/564/375'></img>
     </div>
 
-    <div className = 'px-20 py-10 text-2xl'>Journals</div>
-    <div className = 'flex flex-row px-20'>
+    <div className = 'px-20 py-10 text-4xl'>Publications</div>
+      <div className = 'flex flex-col px-20'>
       
-    </div>
+          <div className = 'text-xl'>Acoustical Design of Walt Disney Concert Hall</div>
+          <div className = 'mb-6 text-gray-600'>Yasuhisa Toyota, Keiji Oguchi, Minoru Nagata</div>
+          <div>Summary: This paper describes the numerous acoustical considerations put into the main hall where concerts are held. For instance, there are small walls among the audience area and the lowest ceiling above the stage. Additionally foam concrete was added behind the wooden panels inside the hall, and there is an air bubble under the stage to help resonate sound. Overall, these careful considerations resulted in the incredible acoustics, and it is clear that the hall was designed around acoustic quality. 
+          <div className= "text-gray-600 mt-4">Toyota, Yasuhisa. “Acoustical Design of the Walt Disney Concert Hall in Los Angeles.” The Journal of the Acoustical Society of America, vol. 105, no. 2, 1999, pp. 987–987., https://doi.org/10.1121/1.425376. Accessed 9 Feb. 2022. </div>
+      </div>
+        <div className = 'text-xl mb-6 mt-8'>Modern Theatres 8.03</div>
+          
+          <div>Summary: The Walt Disney Concert Hall was built to bring life to the barren Bunker Hill in LA and as an iconic landmark for the area. The hall is built around a “vineyard” seating pattern, with the stage centered and the audience on the same level as the performers. The architecture and design of the hall immerses the audience in a concentric community and the performance. 
+          <div className = "text-gray-600 mt-4">Staples, David, and David Hamer. “Walt Disney Concert Hall, 2003 Los Angeles, California, USA.” Modern Theatres, 1950-2020, Routledge, New York, 2021, pp. 380–388. 
+ </div>
+      </div>
+      </div>
 
     </div>
 
